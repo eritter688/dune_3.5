56e2c70f0b5dd6c1cf3cace462f934e09be23af0
Eric <eritter688@gmail.com>
2020-05-11 18:01:34 -0400

    bare bones master defs and prototypes

--- lib/secure/master.ob/defs.h
+++ lib/secure/master.ob/defs.h
@@ -13,17 +13,5 @@
 #define INIT_FILE                          ""
 #define SHUT_FILE                          ""
 
-// simul
-#define SIMUL_EFUN_FILE                    ""
-
-// messages
-#define ENTER_VOID_MESSAGE                 "Everything you see dissolves; luckily, you are transported somewhere else...\n"
-#define VOID_FAIL_MESSAGE                  "Catastrophic error - there is no nothing!\n"
-#define HEART_BEAT_ERROR_MESSAGE           "Game driver tells you: You have no heart beat !\n" \
-                                           "Type 'fix' to restart it.\n"
-#define PLAYER_RUNTIME_ERROR_MESSAGE       "Your sensitive mind notices a wrongness in the fabric of space.\n"
-#define CONNECT_FAIL_MESSAGE               "Game driver too buggy; please come back later.\n"
-#define SHUTTING_DOWN_MESSAGE              "The MUD is closing down now.\n"
-
 #endif // _H_MASTER_DEFS_H_
 

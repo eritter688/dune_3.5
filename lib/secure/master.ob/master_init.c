dd4c016b05c92d0ca7061a5da89fb49346ad7606
eritter688 <eritter688@gmail.com>
2020-05-11 21:16:53 -0400

    work on epilog and preload functions

--- lib/secure/master.ob/master_init.c
+++ lib/secure/master.ob/master_init.c
@@ -9,25 +9,54 @@ void inaugurate_master(int arg) {
 }
 
 /*
- * Return the string to be used as root-uid. Under !strict_euids, the function is expendable.
+ * Return the string to be used as root-uid. 
+ * Under !strict_euids, the function is expendable.
  */
 string get_master_uid() {
     return "1";
 }
 
 /*
- * Evaluate an argument given as option '-f' to the driver on startup from the command line. If
- * several '-f' options are given, this function will be called sequentially with all given arguments.
+ * Evaluate an argument given as option '-f' to the driver on startup 
+ * from the command line. If several '-f' options are given, this 
+ * function will be called sequentially with all given arguments.
  */
 void flag(string arg) {
     debug_message(sprintf("master::flag(string arg) not implemented. Received '%s'.\n", arg));
 }
 
-string *epilog(int eflag) {
-    return;
+string* epilog(int eflag) {
+    string init_file;
+    if(eflag) {
+        debug_message("Skipping init file: "INIT_FILE"\n");
+        return ({});
+    }
+    if((file_size(INIT_FILE) < 0) || (!init_file = read_file(INIT_FILE))) {
+        debug_message("Unable to load init file: "INIT_FILE"\n");
+        return ({});
+    }
+    debug_message("Loading init file: "INIT_FILE"\n");
+    return explode(init_file, "\n");
 }
 
-void preload(string file) {
+/*
+ * Load a file passed in from the epilog function.
+ * 
+ */ 
+void preload(string filename) {
+    int start_time;
+    int finish_time;
+  
+    if(strlen(filename) > 0 && filename[0] != '#') {
+        debug_message(sprintf("Preloading: %s ", filename));
+        start_time = time();
+        if(catch(load_object(filename))) {
+            debug_message(" FAILED\n");
+        } else {
+            finish_time = time();
+            debug_message(sprintf(" %.2fs\n",(current_time-last_time)));
+        }
+    }
     return;
 }
 

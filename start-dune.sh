7b270d58847a986ba8a9da5b43876b2e1d5b2bde
Eric <eritter688@gmail.com>
2020-06-02 11:55:38 -0400

    remove logging from start script

--- start-dune.sh
+++ start-dune.sh
@@ -6,5 +6,7 @@
 
 cd ldbin
 
-./ldmud -DDUNE -D'MUD_NAME="Dune"' --hostname dunemud.net 1> ldmud.log 2>&2 &
+./ldmud
+
+#./ldmud -DDUNE -D'MUD_NAME="Dune"' --hostname dunemud.net 1> ldmud.log 2>&2 &
 #./ldmud -DDUNE -D'MUD_NAME="Dune-2-0"' --hostname dunemud.com >> ldmud.log &
